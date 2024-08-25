52bdcfa1d8702ad2510d186af2c79d94b2c1489c
Mark Dizon <markdizon07@gmail.com>
2024-08-24 23:02:00 -0700

    init

---
+++ game_hud.gd
@@ -0,0 +1,17 @@
+extends CanvasLayer
+
+# TODO: Change this to a dedicated gameover screen
+@onready var game_over_screen = preload("res://start_screen.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_end_game_pressed() -> void:
+	get_tree().call_deferred('change_scene_to_packed', game_over_screen)
---
+++ start_screen.gd
@@ -0,0 +1,29 @@
+extends Control
+
+@export var skip_intro = false
+@onready var settings = preload("res://settings.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_game_pressed() -> void:
+	if (skip_intro):
+		get_tree().change_scene_to_file("res://game.tscn")
+	else:
+		get_tree().change_scene_to_file("res://intro_screen.tscn")
+
+
+func _on_settings_pressed() -> void:
+	get_tree().root.add_child(settings.instantiate())
+	pass
+
+
+func _on_credits_pressed() -> void:
+	get_tree().change_scene_to_file("res://credits.tscn")
